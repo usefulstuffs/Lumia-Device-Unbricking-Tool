41c12e3e96e0f9fdc06ff40c4d5d3e7fe3e280ea
manue <manue@COMPUTER-MANUEL>
2023-10-05 16:09:16 +0200

    Aggiungere i file di progetto.

---
+++ Lumia Device Unbricking Tool/Form1.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace Lumia_Device_Unbricking_Tool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(220, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(335, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Lumia Device Unbricking Tool";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(6, 21);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(661, 27);
+            this.textBox1.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 102);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(776, 55);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Path to thor2.exe";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(685, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Location = new System.Drawing.Point(12, 173);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(776, 55);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Hexadecimal (.hex) file for recovery";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(685, 24);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Browse";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(6, 21);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(661, 27);
+            this.textBox2.TabIndex = 1;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button3);
+            this.groupBox3.Controls.Add(this.textBox3);
+            this.groupBox3.Location = new System.Drawing.Point(12, 245);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(776, 55);
+            this.groupBox3.TabIndex = 4;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "msimage.mbn file for recovery";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(685, 24);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Browse";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(6, 21);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(661, 27);
+            this.textBox3.TabIndex = 1;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.button4);
+            this.groupBox4.Controls.Add(this.textBox4);
+            this.groupBox4.Location = new System.Drawing.Point(12, 321);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(776, 55);
+            this.groupBox4.TabIndex = 5;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Firmware flash (.ffu) file";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(685, 24);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "Browse";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(6, 21);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(661, 27);
+            this.textBox4.TabIndex = 1;
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.ForeColor = System.Drawing.Color.Red;
+            this.button5.Location = new System.Drawing.Point(18, 389);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(115, 49);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "UNBRICK";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(15, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(754, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "WARNING! Incorrectly setting up these values can brick your phone even more. Use " +
+    "this tool only if you know what you\'re doing";
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.ForeColor = System.Drawing.Color.Lime;
+            this.button6.Location = new System.Drawing.Point(149, 389);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(115, 49);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "HELP";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel1.Location = new System.Drawing.Point(409, 418);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(379, 20);
+            this.linkLabel1.TabIndex = 9;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Get Recovery/Firmware files - lumiafirmware.com";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Lumia Device Unbricking Tool - lumiafirmware.com";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ Lumia Device Unbricking Tool/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Lumia_Device_Unbricking_Tool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog thor2ofd = new OpenFileDialog();
+            thor2ofd.CheckFileExists = true;
+            thor2ofd.Filter = "THOR2 executable file|thor2.exe";
+            thor2ofd.Multiselect = false;
+            thor2ofd.Title = "Select thor2.exe file";
+            if (thor2ofd.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = thor2ofd.FileName;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog hexofd = new OpenFileDialog();
+            hexofd.CheckFileExists = true;
+            hexofd.Filter = "Hexadecimal recovery file|*.hex";
+            hexofd.Multiselect = false;
+            hexofd.Title = "Select hexadecimal recovery file";
+            if (hexofd.ShowDialog() == DialogResult.OK)
+            {
+                textBox2.Text = hexofd.FileName;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog thor2ofd = new OpenFileDialog();
+            thor2ofd.CheckFileExists = true;
+            thor2ofd.Filter = "msimage.mbn|*.mbn";
+            thor2ofd.Multiselect = false;
+            thor2ofd.Title = "Select msimage.mbn recovery file";
+            if (thor2ofd.ShowDialog() == DialogResult.OK)
+            {
+                textBox3.Text = thor2ofd.FileName;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog thor2ofd = new OpenFileDialog();
+            thor2ofd.CheckFileExists = true;
+            thor2ofd.Filter = "Firmware flash file|*.ffu";
+            thor2ofd.Multiselect = false;
+            thor2ofd.Title = "Select firmware flash file";
+            if (thor2ofd.ShowDialog() == DialogResult.OK)
+            {
+                textBox4.Text = thor2ofd.FileName;
+            }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            try
+            {
+                Process.Start("http://lumiafirmware.com");
+            }
+            catch
+            {
+                MessageBox.Show("We cannot open the browser. To gather firmware files, copy the link below in your browser:\n\nlumiafirmware.com","Lumia Device Unbricking Tool",MessageBoxButtons.OK,MessageBoxIcon.Information);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            const string quote = "\"";
+            if (textBox1.Text == "" || !File.Exists(textBox1.Text))
+            {
+                MessageBox.Show("The path to thor2.exe field cannot be empty or is invalid.","Lumia Device Unbricking Tool",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+            else if (textBox2.Text == "" || !File.Exists(textBox2.Text))
+            {
+                MessageBox.Show("The hexadecimal recovery file field cannot be empty or is invalid.", "Lumia Device Unbricking Tool", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (textBox3.Text == "" || !File.Exists(textBox3.Text))
+            {
+                MessageBox.Show("The msimage.mbn file for recovery field cannot be empty or is invalid.", "Lumia Device Unbricking Tool", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (textBox4.Text == "" || !File.Exists(textBox4.Text))
+            {
+                MessageBox.Show("The firmware flash (.ffu) file field cannot be empty or is invalid.", "Lumia Device Unbricking Tool", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                MessageBox.Show("Flashing will start, don't disconnect Lumia during this procedure. Disconnecting may cause the permanent brick of the device.","Lumia Device Unbricking Tool - Warning",MessageBoxButtons.OK,MessageBoxIcon.Warning);
+                // For debugging the commands it's going to run:
+                // File.WriteAllText(@"C:\LDUT_debug.txt", $@"{quote}{textBox1.Text}{quote} -mode emergency -hexfile {quote}{textBox2.Text}{quote} -mbnfile {quote}{textBox3.Text}{quote} -ffufile {quote}{textBox4.Text}{quote}");
+                Process.Start(textBox1.Text,$@"-mode emergency -hexfile {quote}{textBox2.Text}{quote} -mbnfile {quote}{textBox3.Text}{quote} -ffufile {quote}{textBox4.Text}{quote}").WaitForExit();
+                MessageBox.Show("Flashing finished. Check now your Lumia's screen.\n\nIf it's in a Green screen, forcedly reboot it by pressing and holding volume down+power button.\n\nIf it's still in a red screen, try again.", "Lumia Device Unbricking Tool - Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($@"How to use this tool:{Environment.NewLine}{Environment.NewLine}Path to thor2.exe - Install Windows Device Recovery Tool and search in its installation folder (usually C:\Program Files (x86)\Microsoft Care Suite\Windows Device Recovery Tool).{Environment.NewLine}{Environment.NewLine}Hexadecimal (.hex) file for recovery - The .hex file you get from lumiafirmware.com under the emergency files section.{Environment.NewLine}{Environment.NewLine}msimage.mbn file for recovery - You get this file from lumiafirmware.com under the emergency files section{Environment.NewLine}{Environment.NewLine}Firmware flash (.ffu) file - This is the firmware file. You can get it from both lumiafirmware.com and Windows Device Recovery Tool cache from a previous update.","Lumia Device Unbricking Tool - Help",MessageBoxButtons.OK,MessageBoxIcon.Information);
+        }
+    }
+}
